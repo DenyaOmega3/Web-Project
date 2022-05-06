b9278676a712805bd06ccd86eb389894c6525939
DenyaOmega3 <rpoj18.1.denya.lobanov1@gmail.com>
2022-05-06 12:43:43 +0300

    06.05.22, aside tag displaying fixed

--- dynamics.js
+++ dynamics.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     $('#navigationAlternative').on('click', function() {
         $('aside').show();
+        $('body').css({"overflow": "hidden"});
     });
 
     $('#close').on('click', function() {
         $('aside').hide();
+        $('body').css({"overflow": "visible"});
     });
 });
 
